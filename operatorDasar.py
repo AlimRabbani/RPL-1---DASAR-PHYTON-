1add9d23bbe0489bbfcfaa5e93f22e645a6d759a
AlimRabbani <alimmanurung18@gmail.com>
2022-10-21 20:31:06 +0800

    rpl day 5

---
+++ operatorDasar.py
@@ -0,0 +1,17 @@
+#sisa bagi / modulus 
+angka1 = 14 
+angka2 = 4 
+hasilnya = angka1 % angka2 
+print ("hasilnya adalah : " , hasilnya) 
+
+# pangkat  
+a = 10 
+b = 2
+hasil = a**b 
+print ("hasilnya adalah :" , hasil)  
+
+#pembagian bulat 
+c=10 
+d=3 
+hasil = c//d 
+print("hasil :" , hasil) 
