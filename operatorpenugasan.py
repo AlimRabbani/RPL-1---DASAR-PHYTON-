4cb66f5eb2b5915727e15704651bf2524c8affc0
AlimRabbani <alimmanurung18@gmail.com>
2022-10-23 00:27:30 +0800

    rpl day 7

---
+++ operatorpenugasan.py
@@ -0,0 +1,35 @@
+ #operator perbandingan 
+a = 10 #inputkan nilai a = 10 
+
+#operator ini bisa kita kombinasikan dengan operator oprator lainnya contohnya operator ARITMATIKA 
+#Mari kita mulai  
+#penjumlahan 
+a +=2 # atau a = a + 10  
+print("nilai  a :" , a)  
+
+#pengurangan 
+a -= 4 # atau a = a - hasil dari penjumlahan di atas 
+print("nilai a : " , a)  
+
+#perkalian 
+a *= 5 # atau a = a * hasil dari pengurangan di atas  
+print("nilai a :" , a)  
+
+#pembagian 
+
+a /= 2 # atau a = a :/ hasil perkalian di atas  
+print(" nilai a :" , a)  
+
+#pangkat ** 
+a **= 2 # atau a = a ** hasil perkalian di atas 
+print("nilai a : " , a)  
+
+#dst 
+#DON'T GIVE UP
+
+
+
+
+
+
+
