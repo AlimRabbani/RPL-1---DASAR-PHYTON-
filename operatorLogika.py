87320dda23b5225b5a58a73f2c076f3e5b19d311
AlimRabbani <alimmanurung18@gmail.com>
2022-10-24 18:22:38 +0800

    rpl day 8

---
+++ operatorLogika.py
@@ -0,0 +1,14 @@
+x = True 
+y = False 
+
+#logika AND 
+z = x and y 
+print("true and false z : " , z ) 
+
+#logika OR 
+z = x or y 
+print("true or false z :" , z) 
+
+#logika NOT 
+z = not x 
+print("not true :" , z)
