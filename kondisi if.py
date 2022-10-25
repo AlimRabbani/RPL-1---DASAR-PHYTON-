41b543e559dac72057ae79f93d0d12488f25e82f
AlimRabbani <alimmanurung18@gmail.com>
2022-10-25 19:48:21 +0800

    rpl day 9

---
+++ kondisi if.py
@@ -0,0 +1,13 @@
+#kondisi if adalah kondisi yang akan dieksekusi oleh program jika bernilai benar atau TRUE
+
+nilai = 7
+#jika kondisi benar/TRUE maka program akan mengeksekusi perintah dibawahnya
+if(nilai > 5): 
+    print("tuju lebih besar dari angka lima")#kondisi benar , dieksekusi 
+
+#jika kondisi salah/FALSE maka program tidak akan mengeksekusi perintah dibawahnya    
+if(nilai > 8):  
+    print("delapan lebih kecil dari angka 7")#kondisi salah , maka tidak akan dieksekusi
+
+
+    
