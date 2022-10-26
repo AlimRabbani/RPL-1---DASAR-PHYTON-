64af484bf7dec83beace8fe54b9fcde0a5402e6b
AlimRabbani <alimmanurung18@gmail.com>
2022-10-26 18:37:45 +0800

    rpl day 10

---
+++ kondisi if Else.py
@@ -0,0 +1,7 @@
+nilai = 7
+#jika pernyataan pada if bernilai TRUE maka akan dieksekusi pada if,tetapi jika false kode pada else yg akan dieksekusi
+if (nilai > 5): 
+    print("selamat anda  lulus") 
+
+else : 
+    print("maaaf anda tidak lulus ")
