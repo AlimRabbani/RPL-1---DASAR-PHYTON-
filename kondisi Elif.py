059a2a1b0be226b3e8e23109c5aa1cb05a558519
AlimRabbani <alimmanurung18@gmail.com>
2022-10-27 12:48:13 +0800

    rpl day 11

---
+++ kondisi Elif.py
@@ -0,0 +1,16 @@
+#contoh penggunaan kondisi elif PYTON
+hari_ini ="minggu" 
+if(hari_ini == "senin"): 
+    print("saya akan kuliah") 
+elif(hari_ini == "selasa"):
+    print("saya akan kuliah")    
+elif(hari_ini == "rabu"): 
+    print("saya akan kuliah") 
+elif(hari_ini == "kamis"): 
+    print("saya akan kuliah") 
+elif(hari_ini == "jumat"): 
+    print("saya akan kuliah ")  
+elif(hari_ini == "sabtu"): 
+    print("saya akan kuliah") 
+elif(hari_ini == "minggu"): 
+    print(" saya akan libur")                  
