6cd800e3622ea7208d95672aa32ae1df220e8a44
AlimRabbani <alimmanurung18@gmail.com>
2022-10-28 16:47:36 +0800

    rpl day 12

---
+++ list.py
@@ -0,0 +1,23 @@
+#contoh list
+#list kosong 
+list_kosong =[]  
+
+#list yg berisi kumpulan string 
+list1_buah =['pisang','nanas','apel','jeruk','mangga'] 
+
+#list yg berisi kumpulan integer 
+list2_nilai =[80,70,90,60] 
+
+#list campuran 
+list3_alim =[150 , 33.33 ,'presiden sukarno' , False]  
+
+#output 
+print('list_kosong :',list_kosong) 
+print('list1_buah :' ,list1_buah) 
+print('list2_nilai :' , list2_nilai) 
+print('list3_alim :' ,list3_alim)  
+
+#kita juga bisa menampilkan salah satu indeks pada list di atas 
+#conton 
+print(list1_buah[4])#maka akan menampilkan [MANGGA] 
+
