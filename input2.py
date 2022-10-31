e9948ef6500340776cf2acf977db6975f1980724
AlimRabbani <alimmanurung18@gmail.com>
2022-10-31 13:31:43 +0800

    rpl day 15

---
+++ input2.py
@@ -0,0 +1,10 @@
+#membuat biodata menggunakan fungsi input PHYTON
+print("--------------------") 
+print("biodata deserhana ") 
+
+nama = input("masukkan nama :") 
+alamat = input("masukkan alamat anda :")  
+hobi = input("masukkan hobi :") 
+umur = input("inputkan umur :") 
+
+print("hallo , nama saya %s ,alamat %s ,hobi saya %s dan umur saya %s tahun" % (nama ,alamat , hobi , umur))
