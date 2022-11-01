339cae6f5c965aee30fa27c398edc4cb09f4a3b7
AlimRabbani <alimmanurung18@gmail.com>
2022-11-01 16:54:26 +0800

    rpl day 16

---
+++ kakulatorSederhana.py
@@ -0,0 +1,7 @@
+#kakulator sederhana
+a = input("masukkan nilai a: ") #perintah masukan nilai dari keyboard, yang nilainya akan disimpan pada variabel a 
+b = input("masukkan nilai b: ") #perintah masukan nilai dari keyboard, yang nilainya akan disimpan pada variabel c 
+
+c = int(a) + int(b) #operasi penjumlahan variabel a dan b, dimana dilakukan konversi menjadi type integer pada nilai variabel a dan b, nilai outputnya disimpan pada variabel c
+ 
+print(c) #perintah menampilkan nilai yang disimpan pada variabel c
