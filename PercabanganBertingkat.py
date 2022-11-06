a12603ab556248f44fc1b40298a10cde297d4362
AlimRabbani <alimmanurung18@gmail.com>
2022-11-06 17:16:34 +0800

    rpl day 21

---
+++ PercabanganBertingkat.py
@@ -0,0 +1,25 @@
+#PERCABANGAN BERTINGKAT
+#percabangan bertingkat atau lebih jelasnya if dalam if 
+#if dalam if kalo dalam dunia perbakso-an itu kayak bakso dalam bakso(Bakso beranak , bakso lava , bakso besar)
+# seperti itu kira - kira 
+# Dan ini contoh dasar programnya   
+nilai = int(input("masukkan nilai  :"))  
+usia = int(input("Masukkan usia :")) 
+
+if (nilai >= 75): #bakso beranak
+ if (usia < 16) : #ini anaknya 
+    print("Selamat adek, kamu lulus dalam mencintai orang yang salah!")
+    
+ else: 
+    print("Selamat kakak , kakak juga lulus kayak adek! ")  
+else :
+   if(usia<16):  
+     print("Mohon maaf adek , mungkin belum jodoh!")
+   else : 
+    print("mohon maaf kakak , kayaknya kakak belum jodoh juga kayaknya!")  
+
+#singkatnya program di atas akan memeriksa terlebih dahulu apakah nilai yang dimasukkan lulus atau tidak .
+#setelah itu, program akan memeriksa usia , apakah dia akan di sapa dengan kakak ataukah dengan adek. 
+#jika kondisi pertama(if) tidak terpenuhi maka program akan mengeksekusi ke kondisi yang kedua(else)
+#PercabanganBertingkat Said : The value of a man is not in his charming face, has everything and is cool.. But the value of a man lies in how he respects a woman
+#That's it for day , Thank you
