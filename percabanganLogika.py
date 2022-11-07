55e4161d8aa31efdf1c12b1dc160d6d72014212b
AlimRabbani <alimmanurung18@gmail.com>
2022-11-07 13:21:58 +0800

    rpl day 22

---
+++ percabanganLogika.py
@@ -0,0 +1,13 @@
+#PERCABNGAN LOGIKA PHYTON / operator(identity operators)
+#CONTOH 
+Menu_makanan_yang_tersedia = ['mie ayam','bakso lava','Bakso beranak','ayam bakar','ayam rica-rica'] 
+Menu_makanan_yang_dicari = input("masukkan menu pesanan anda  : ") 
+
+
+if(Menu_makanan_yang_dicari in Menu_makanan_yang_tersedia): # in adala operator / Bernilai true jika nilai/buah/item yang di cari ada dalam variabel/himpunan
+#jadi operator (in) ini kayak penghubung gitu dengan dua variabel di atas 
+    print("menu makanan  tersedia") 
+else :
+    print("mohon maaf makanan  tidak tersedia") 
+#thanks
+#HAL TERINDAH
