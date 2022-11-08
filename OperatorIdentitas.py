6ee154a1dc0531660562f131f2b089f99e41932e
AlimRabbani <alimmanurung18@gmail.com>
2022-11-08 13:44:08 +0800

    rpl day 23

---
+++ OperatorIdentitas.py
@@ -0,0 +1,29 @@
+#ok skuy lanjut kita di : OPERATOR IDENTITAS PHYTON 
+#kita langsung aja ke pembahasan dan contoh operator identitas phyton ini , gaskeun ! 
+
+#operator ini terdiri dari dua jenis : 
+# 1 . is  
+# is not 
+#berikut contoh penggunaannya :  
+a = 5 
+b = 5 
+c = 7  
+print("a is b :", a is b) # True
+print("b is a :", b is a) # true
+print("c is a :" , c is a) # False
+print("b is c :" , b is c) # False  
+#jadi seperti ini contoh untuk program operator is 
+print(" a is not b :", a is not b) # False
+print("b is not a :", b is not a) # False
+print(" c is not a :" , c is not a) # True
+print("b is not c :" , b is not c) # True  
+
+#belum paham ? yah masa belom paham 
+#ok kita ke contoh berikutnya 
+x = "lufffy" 
+y = "luffy" 
+print("x is y :", x is y) #False
+print("y is not x :" , y is not x) #True
+#Semoga bermanfaat 
+#sekian untuk hari ini 
+#Terimaksih 
