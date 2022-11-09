1cbe0d5f30512497adf3e65d68cdcb1ad520d8dc
AlimRabbani <alimmanurung18@gmail.com>
2022-11-09 22:50:42 +0800

    rpl day 24

---
+++ operatorKeanggotaan.py
@@ -0,0 +1,19 @@
+#OPERATOR KEANGGOTAAN PHYTON / MEMBERSHIP OPERATOR 
+#UNTUK OPERATOR KEANGGOTAAN KALI INI KITA AKAN MEMBAHASA MENGENAI OPERATOR in DAN not in 
+# in/not in 
+#berikut contoh penggunaan operator in / not in : 
+
+Dunia = 'cakrawala' 
+print("Dunia :",Dunia) 
+print("a in Dunia :" , 'a' in Dunia)#TRUE
+print(" c not in Dunia :" , 'c' not in Dunia)#FALSE 
+#sudah paham ? 
+#ok kalau masih beum paham mengenai operator ini kita akan ke contoh yang kedua 
+Huruf = ['k','l','m','n','o'] 
+print("Huruf :" , Huruf)
+print(" k in Huruf :" , 'k' in Huruf)#akan bernilai true karena data/nilai yang di cari ada dalam kotak data , paham kan ! 
+print("z not in Huruf :" , 'z' not in Huruf)#akan bernilai True karena data/nilai yang di cari tidak ada dalam kotak data !
+#jadi operator (not in) ini akan bernilai True ketika kita mencari data/nilai yang tidak ada dalam kotak data(variabel) 
+#seperti itu contohnya yah gays 
+#Dan untuk operator (in) akan bernilai true jika nilai/data yang kita cari ada dalam kotak data , sebaliknya akan bernilai false jika nilai/data yang di cari tidak ada dalam kotak data(variabel)
+
