6dec290e9dcdcc5653fbc60ab9386f9ede153bab
AlimRabbani <alimmanurung18@gmail.com>
2022-11-10 14:07:18 +0800

    rpl day 25

---
+++ Tuple.py
@@ -0,0 +1,38 @@
+#Tipe data TUPLE phyton 
+#Berikut cara membuat TUPLE phyton 
+
+contoh1 =("ayo","belajar","phyton") 
+contoh2 =(10,20,30,40,50) 
+contoh3 =("PHYTON",100, 4.77 , True) 
+print(contoh1) 
+print(contoh2) 
+print(contoh3)  
+#Nyaris tidak jauh berbeda dengan List. yang membedakan TUPLE dengan LIST itu hanya penggunaan tanda kurungnya saja 
+#tuple()
+#list[] 
+#paham kn , nh berikut pembuktiannya : 
+luffy =["luffy","zoro","sanji",100 , True]#List  
+print(type(luffy))  
+Ussop = ("one","piece",1037,True)#Tuple 
+print(type(Ussop))  
+
+#Berikut cara untuk mengakses nilai/data pada TUPLE 
+zoro = ("one","piece",1037,True) 
+print(zoro[0])#Jadi yang akan muncul adalah indeks ke 0 pada variabel data di atas 
+print(zoro[1])#akan muncul indeks pertama 
+print(zoro[2])#indeks ke 2  
+#jadi seperti itulah cara untuk mengakses nilai/data pada tipe data tuple 
+#mengakses elemen di List juga seperti itu 
+#berikut contohnya :
+Robin =["nico" ,"robin","nami","chopper", 1037 , True]  
+print(Robin[0]) 
+#MENGANTI NILAI PADA TIPE DATA TUPLE 
+#apakah bisa ? sepertinya tidak bisa ! alasannya karena tipe data TUPLE tidak bisa di ubah atau di ganti setelah didefinisikan 
+#mari kita coba :
+franky =("hay","mari","belajar","phyton", 1000, True) 
+franky[2] ='Jimbey' 
+print(franky) 
+#Hasilnya error gays 
+#silahkan di coba , ini juga yang membedakan antara tipe data tuple dengan list karena di list itu kita bisa menganti dan menambakan nilai baru dalam sebuah variabel data .sementara di tuple itu tidak
+#sekian gays untuk hari ini , TERIMAKASIH BANYAK 
+
