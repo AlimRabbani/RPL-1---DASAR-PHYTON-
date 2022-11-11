11c56a5b1e8f4d2849a11c892de689446dc8192b
AlimRabbani <alimmanurung18@gmail.com>
2022-11-11 18:25:56 +0800

    rpl day 26

---
+++ TipeDataSet.py
@@ -0,0 +1,14 @@
+#cara membuat tipe data set phyton 
+luffy = {"ayo","belajar","phyton","di","google"} 
+zoro = {100,200,300,400,500} 
+sanji = {"phyton", 100 ,2.14,True} 
+print(luffy) 
+print(zoro) 
+print(sanji) 
+#pertanyaanya apa kita bisa mengakses indeks pada tpe data set ? 
+#mari kita coba 
+
+ussop = {"phyton","ayo","belajar","bersama"} 
+print(ussop[2]) 
+#ya hasil yang kita dapatkan itu adalah (ERROR)
+#alasannya karena tipe data set ini tidak mimiliki INDEKS 
