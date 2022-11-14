f9e89e7ab4ccd7dc2e7743e0f12ad79429b63600
AlimRabbani <alimmanurung18@gmail.com>
2022-11-14 16:36:03 +0800

    rpl day 29

---
+++ perulanganWhile.py
@@ -0,0 +1,29 @@
+#Contoh program 
+luffy = 1 
+while luffy <=5: 
+    print('one piece')
+    luffy+=1 
+
+#nah apa kita bisa mengakses nilai dari variabel counter ? 
+#ya tentu saja bisa . dengan cara : 
+i = 1 
+while i <= 10:
+    print('luffy', i) 
+    i +=1  
+#seperti itu gays 
+#yg paling penting jangan lupa buat INCREMENTNYA ygy 
+#ok selanjutnya perulangan menurun 
+eka = 10
+while eka > 5: 
+    print("julia",eka)
+    eka -=1 
+
+#neks perulangan For 
+
+
+
+
+
+
+
+
