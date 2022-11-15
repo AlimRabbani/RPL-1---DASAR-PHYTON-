b77efd4db25d5728891810b04815b12343cff201
AlimRabbani <alimmanurung18@gmail.com>
2022-11-15 18:57:57 +0800

    rpl day 30

--- kondisi if Else.py
+++ kondisi if Else.py
@@ -1,7 +1,18 @@
-nilai = 7
-#jika pernyataan pada if bernilai TRUE maka akan dieksekusi pada if,tetapi jika false kode pada else yg akan dieksekusi
-if (nilai > 5): 
-    print("selamat anda  lulus") 
+# nilai = 7
+# #jika pernyataan pada if bernilai TRUE maka akan dieksekusi pada if,tetapi jika false kode pada else yg akan dieksekusi
+# if (nilai > 5): 
+#     print("selamat anda  lulus") 
 
-else : 
-    print("maaaf anda tidak lulus ")
+# else : 
+#     print("maaaf anda tidak lulus ") 
+
+    #nilai 
+nilai = 10 
+if nilai > 7 :
+        print("hasil :", nilai) 
+elif nilai < 5 : 
+        print("hasil :", nilai) 
+elif nilai != 9 : 
+        print("hasil :", nilai) 
+else :
+      print("eror")
---
+++ perulanganFor.py
@@ -0,0 +1,21 @@
+#perulangan For phyton 
+warna = ['mera','kuning','hijau','biru'] 
+for i in warna : 
+    print(i)
+#perulangan for juga bisa kita pakai untuk tipe data lain , contohnya tipe data set
+warna1 = {"merah","hitam","orange","coklat"} 
+for i in warna1 : 
+    print(i) 
+#penggunaan function range() 
+for i in range(5): 
+    print(i) 
+
+#latihan 
+for i in range(5):
+    print("*"*i) 
+#latihan2 
+luffy = 1 
+while luffy <= 5: 
+    print("*"*luffy)
+    luffy +=1 
+#sekian gys
