dfa39a4aed9ffd131a60c1da91273adb781f920d
AlimRabbani <alimmanurung18@gmail.com>
2022-11-17 17:40:17 +0800

    rpl day 32

---
+++ fungsi.py
@@ -0,0 +1,77 @@
+#contoh 1 
+def siapa_karakterOverPower_onePiece(): #fungsi 
+    print('luffy','zoro','sanji') 
+
+siapa_karakterOverPower_onePiece() 
+siapa_karakterOverPower_onePiece() 
+siapa_karakterOverPower_onePiece()   
+
+#contoh 2  
+#kita akan membuat beberapa function 
+def alim(): #nama funngsiya bebas yah 
+    print('hay lim') 
+def one_piece(): 
+    print('oy luffy') 
+def naruto(): 
+    print('aku pasti akan menjadi hokage') 
+
+alim() 
+one_piece() 
+naruto()  
+
+#ok neks variabel dalam function 
+#contoh 
+def hitung_luas_segitiga(): 
+    alas = 5
+    tinggi = 10
+    luas = (alas * tinggi) /2 
+    print('luas segitiga = ', luas)  
+
+hitung_luas_segitiga() 
+ 
+#latihan menghitung luas segitiga dan volume : 
+
+def hitung_luas(alas , tinggi , luas): 
+    alas =() 
+    tinggi = () 
+    luas = (alas * tinggi ) / 2 
+      
+def volume_balok(alas,tinggi,volume):  
+    alas = () 
+    tinggi = () 
+    volume =(alas * tinggi) / 2 
+   
+    alas = (5) 
+    tinggi = (10) 
+    
+# hitung_luas() 
+# volume_balok()  
+#kalo teman teman mengikuti cara yg di atas otomatis program teman" akan error karna itu struktur programmnya salah
+#teman teman harus mengikuti contoh program yang saya share di bawah ini 
+ 
+def hitung_luas(): 
+    alas = 10 
+    tinggi = 5 
+    luas = (alas * tinggi) / 2 
+    print('luasnya =' , luas) 
+def hitung_volume(): 
+    a = 5 
+    b = 2 
+    volume = (a * b) / 2 
+    print('volumenya = ' , volume) 
+
+hitung_luas() 
+hitung_volume() 
+
+#sekian untuk hari ini 
+#terimakasih 
+
+
+
+   
+
+
+
+
+
+
