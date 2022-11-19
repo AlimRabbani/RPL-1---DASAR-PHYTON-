640304b9eafffe5c7dc11b25ec27020085d0e894
AlimRabbani <alimmanurung18@gmail.com>
2022-11-19 12:56:54 +0800

    rpl day 34

---
+++ fungsi3.py
@@ -0,0 +1,36 @@
+#latihan , contoh soal 
+#1 .hitinglah luas segitiga(L) 
+#2. hitunglah panjang volume balok (V(s*s*s)) 
+#3.hitunglah panjang balok(V(p)(l)(t)) 
+#carilah hasil luas segita , panjang volume balok , panjang balok menggunakan fungsi 
+#buatlah programnya sesederhana mungkin ! 
+
+def luas_segitiga(alas , tinggi):  
+    luas = (alas * tinggi) / 2 
+    print('luas segitiga adalah :' , luas) 
+
+luas_segitiga(10 , 2) 
+#ya hasilnya adalan 10 karna 10 x 2 = 20 : 2 = 10 
+#maap kalo salah  
+# volume balok  
+
+def volume_kubus(panjang , lebar , tinggi):  
+    volume = panjang * lebar * tinggi / 2 
+    print('volume segitiga sebasar :', volume)  
+
+volume_kubus( 10 , 2 , 4 )  
+
+#panjang  balok 
+
+def panjang_balok(panjang , lebar,tinggi): 
+    panjang = panjang * lebar * tinggi / 2 
+    print('panajng segitiga adalah :' , panjang) 
+panjang_balok(2,4,5) 
+ 
+#ini adalah cara yang ketiga untuk mencari sebuah nilai 
+
+
+
+
+  
+
