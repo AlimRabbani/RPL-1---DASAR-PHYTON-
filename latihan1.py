bd8429aac172f00bbb3cba72bf3316f12ff2d88d
AlimRabbani <alimmanurung18@gmail.com>
2022-11-20 18:43:42 +0800

    rpl day 35

---
+++ latihan1.py
@@ -0,0 +1,37 @@
+#latihan membuat program pengecekan mata kuliah menggunakan if / elif / else 
+jadwal_kuliah = input('masukkan jadwal kuliah anda :') 
+
+if jadwal_kuliah == 'senin': 
+    print('mata kuliah hari ini kosong') 
+elif jadwal_kuliah == 'selasa': 
+    print('mata kuliah hari ini :') 
+    print("arsitektur dan organisasi komputer ===>>> jam : 09:10 - 10 : 50") 
+    print('Pengembangan multimedia ===>>> jam : 10:50 - 12 -30') 
+    print('pemrograman berorientasi objek ====>>>  jam 13 : 00 - 14 - 40') 
+
+elif jadwal_kuliah == 'rabu': 
+    print('mata kuliah hari ini :') 
+    print("pemrograman web 1 ===>>> jam : 07:30 - 09 : 10") 
+    print('aljabar linear ===>>> jam : 09:10 - 10 -50') 
+    print('aplikasi komputer ===>>>  jam 13 : 00 - 14 - 40') 
+    print('IOT ====>>> jam 14:40 - 16 : 20') 
+elif jadwal_kuliah == 'kamis': 
+     print('mata kuliah hari ini :') 
+     print("basis data 1 ===>>> jam : 07:30 - 09 : 10") 
+     print('bahasa inggris ===>>> jam : 09:10 - 10 -50')  
+elif jadwal_kuliah == 'jumat': 
+     print('mata kuliah hari ini :') 
+     print("interaksi manusia dan komputer ===>>> jam : 09:10 - 10 : 50")  
+elif jadwal_kuliah == 'sabtu': 
+     print('mata kuliah hari ini :') 
+     print('mata kuliah hari ini kosong') 
+elif jadwal_kuliah == 'minggu': 
+    print('luburr')  
+#terimakasih 
+
+
+
+    
+    
+
+
