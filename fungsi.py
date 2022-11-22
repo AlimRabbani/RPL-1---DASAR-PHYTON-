348a2c877494d7428cf018b22a012a314fab760e
AlimRabbani <alimmanurung18@gmail.com>
2022-11-22 21:29:16 +0800

    tugas 2

--- fungsi.py
+++ fungsi.py
@@ -1,33 +1,33 @@
 #contoh 1 
-def siapa_karakterOverPower_onePiece(): #fungsi 
-    print('luffy','zoro','sanji') 
+# def siapa_karakterOverPower_onePiece(): #fungsi 
+#     print('luffy','zoro','sanji') 
 
-siapa_karakterOverPower_onePiece() 
-siapa_karakterOverPower_onePiece() 
-siapa_karakterOverPower_onePiece()   
+# siapa_karakterOverPower_onePiece() 
+# siapa_karakterOverPower_onePiece() 
+# siapa_karakterOverPower_onePiece()   
 
 #contoh 2  
 #kita akan membuat beberapa function 
-def alim(): #nama funngsiya bebas yah 
-    print('hay lim') 
-def one_piece(): 
-    print('oy luffy') 
-def naruto(): 
-    print('aku pasti akan menjadi hokage') 
+# def alim(): #nama funngsiya bebas yah 
+#     print('hay lim') 
+# def one_piece(): 
+#     print('oy luffy') 
+# def naruto(): 
+#     print('aku pasti akan menjadi hokage') 
 
-alim() 
-one_piece() 
-naruto()  
+# alim() 
+# one_piece() 
+# naruto()  
 
 #ok neks variabel dalam function 
 #contoh 
-def hitung_luas_segitiga(): 
-    alas = 5
-    tinggi = 10
-    luas = (alas * tinggi) /2 
-    print('luas segitiga = ', luas)  
+# def hitung_luas_segitiga(): 
+#     alas = 5
+#     tinggi = 10
+#     luas = (alas * tinggi) /2 
+#     print('luas segitiga = ', luas)  
 
-hitung_luas_segitiga() 
+# hitung_luas_segitiga() 
  
 #latihan menghitung luas segitiga dan volume : 
 
@@ -44,8 +44,8 @@ def volume_balok(alas,tinggi,volume):
     alas = (5) 
     tinggi = (10) 
     
-# hitung_luas() 
-# volume_balok()  
+hitung_luas() 
+volume_balok()  
 #kalo teman teman mengikuti cara yg di atas otomatis program teman" akan error karna itu struktur programmnya salah
 #teman teman harus mengikuti contoh program yang saya share di bawah ini 
  
---
+++ latihan3.py
@@ -0,0 +1,63 @@
+# alpa =int(input('masukkan jumla kehadiran anda =')) 
+# gaji = int(input("masukkan gaji anda :"))
+# pot_alpa = 25000 
+# pajak_karyawan = 2.5/100  
+
+# potongan_pajak_karyawan = gaji*pajak_karyawan
+# gaji_kotor_karyawan =gaji * potongan_pajak_karyawan
+# gaji_bersih_karyawan = gaji - potongan
+
+
+# if  alpa > 5 :   
+#         potongan = pot_alpa * alpa 
+
+# else : 
+#         potongan = 0*alpa   
+
+# potongan_pajak_karyawan = gaji*pajak_karyawan
+# gaji_kotor_karyawan =gaji * potongan_pajak_karyawan
+# gaji_bersih_karyawan = gaji - potongan
+
+
+
+# print("masukkan jumla alpa = ", alpa) 
+# print("masukkan jumla gaji =" ,gaji) 
+# print('pajak =', potongan_pajak_karyawan) 
+# print('gaji kotor=',gaji_kotor_karyawan) 
+# print('gaji bersih =',gaji_bersih_karyawan)   
+
+#latihan 2 
+alpa =int(input('masukkan jumla kehadiran anda =')) 
+gaji = int(input("masukkan gaji anda :"))
+pot_alpa = 25000 
+pajak_karyawan = 2.5/100    
+
+if  alpa > 5 :   
+        potongan_pajak = pot_alpa*alpa 
+
+else : 
+        potongan_pajak = 0*alpa    
+
+pot_pajak_karyawan =  pot_alpa*alpa 
+gaji_kotor_karyawan = gaji*pajak_karyawan
+gaji_bersih_karyawan = gaji-potongan_pajak
+
+        
+        
+
+
+print("masukkan jumla alpa = ", alpa) 
+print("masukkan jumla gaji =" ,gaji) 
+print('pajak =', pot_pajak_karyawan) 
+print('gaji kotor=',gaji_kotor_karyawan) 
+print('gaji bersih =',gaji_bersih_karyawan)   
+
+
+
+
+
+
+
+
+
+
