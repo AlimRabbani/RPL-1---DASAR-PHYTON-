ca35b61c1199aeb26b09c8691ac91351e1660462
AlimRabbani <alimmanurung18@gmail.com>
2022-11-28 20:16:32 +0800

    rpl day 44

---
+++ break.py
@@ -0,0 +1,28 @@
+#contoh code program perintah break phyton 
+
+# i = 1 
+# while i <= 10: 
+#     print(i,'x',i,'=',i*i) 
+#     i += 1  
+
+#contoh 2 
+# k = 1
+# while k <=10: 
+#    print(k,'x',k,'=',k*k) 
+#    if k == 5: 
+#     break
+#    print(k,'x',k,'=',k*k) 
+#    k += 1   
+
+#contoh 3  n
+for a in range(1,11): 
+     print(a,'x',a,'=',a*a)  
+     if a == 5: 
+       break 
+     
+   
+
+
+
+  
+
