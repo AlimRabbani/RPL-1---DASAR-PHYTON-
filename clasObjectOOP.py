4882b1b6ddfaaec00f2fbfb9ca765c74eda0b6be
AlimRabbani <alimmanurung18@gmail.com>
2022-12-03 04:14:07 +0800

    clas object OOP

---
+++ clasObjectOOP.py
@@ -0,0 +1,31 @@
+class Superman: #temolate/clas 
+    pass 
+
+
+superman1 = Superman() # object 
+superman2 = Superman() 
+superman3 = Superman()  
+
+superman1.name = "alim rabbani" 
+superman1.healt = 1000  
+superman1.power = 10000 
+superman1.power2 = "terbang" 
+superman1.power3 = "kuat" 
+superman1.power4 = "menolong orang" 
+#ok untuk superman1 sebagai object , name /healt/powe sebagai atribut dan yang terakhir power 2 - 4 sebagai perilaku 
+#maaf yh 
+superman2.name = "Mel" 
+superman2.healt = 200 
+superman2.power = 500
+
+superman3.name = "luffy" 
+superman3.healt = 1000000000000 
+superman3.power = "over power"  
+
+print(superman1) 
+print(superman1.__dict__) #buat manggil semuah atribut superman1 
+print(superman1.name , superman1.helat,superman1.power) # pemanggilan atributnya juga bisa seperti ini silahka di coba 
+
+# ok neks kita bakal bahas atribut 
+#terimaksih 
+
