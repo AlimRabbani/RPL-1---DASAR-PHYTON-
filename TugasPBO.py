3db077cf2b546de970e1febcec2ead1459ddb55d
AlimRabbani <alimmanurung18@gmail.com>
2022-12-13 20:43:03 +0800

    rindu rumah

---
+++ TugasPBO.py
@@ -0,0 +1,39 @@
+class Luas: 
+    def __init__(self, a,t): 
+        self.alas = a 
+        self.tinggi = t 
+    def __str__(self):
+        luasSegitiga = 0.5 *self.alas * self.tinggi 
+        return f'segitiga(alas={self.alas}={self.tinggi} luas={"luas egitiga adalah ",luasSegitiga})' 
+
+    def __init__(self, a,r): 
+        self.luas = a 
+        self.jari_jari = r
+    def __str__(self):
+        luasLingkaran= 0.5 *self.luas* self.jari_jari
+        return f'segitiga(alas={self.luas}={self.jari_jari} luas={"luas lingkaran adalah ",luasLingkaran})' 
+
+    def __init__(self, p , l , t): 
+        self.panjang = p
+        self.lebar= l 
+        self.tinggi = t 
+    def __str__(self):
+        luasVolume = 0.5 *self.panjang * self.lebar * self.tinggi  
+        return f'segitiga(alas={self.panjang}={self.lebar}={self.tinggi} luas={"luas kubus adalah ",luasVolume})'  
+    
+    def __init__(self, p , l ,t): 
+        self.panjang = p
+        self.lebar = l
+        self.tinggi = t
+    def __str__(self):
+        luasBalok = 0.5 *self.panjang * self.lebar * self.tinggi
+        return f'segitiga(alas={self.alas}={self.tinggi} luas={"luas egitiga adalah ",luasBalok})' 
+
+    a =(20 ,3,7) 
+
+    print(a)
+    
+ 
+
+    
+
---
+++ zoro.py
@@ -0,0 +1,27 @@
+#program ini di buat oleh seorang mahasiswa yang sedang merindukan suasana rumah/pen pulkam  
+
+mk = input("masukkan mata kuliah :") 
+
+if mk == "Bahasa inggris": 
+    print("MK sudah selesai , sialhaka tunggu nilai akhir anda") 
+elif mk == "Pemrograman web":
+    print("MK sudah selesai , sialhaka tunggu nilai akhir anda") 
+elif mk == "Aljabar linear":
+    print("MK sudah selesai , sialhaka tunggu nilai akhir anda")
+elif mk == "Internet of things": 
+    print("MK belum selesai , sialhaka lanjutkan") 
+elif mk == "Basis data": 
+    print("MK belum selesai , sialhaka lanjutkan") 
+elif mk == "Pemrograman berorientasi objek": 
+    print("MK belum selesai , sialhaka lanjutkan")  
+elif mk == "Arsitektur dan organisasi komputer": 
+    print("MK belum selesai , sialhaka lanjutkan") 
+elif mk == "Pengembangan multimedia":
+    print("MK belum selesai , sialhaka lanjutkan") 
+elif mk == "Aplikasi komputer": 
+    print("MK belum selesai , sialhaka lanjutkan") 
+elif mk == "Interaksi manusia dan komputer": 
+    print("MK belum selesai , sialhaka lanjutkan") 
+else : 
+    print("MK tidak di temukan mohon periksa kembali , terimakasih")  
+
