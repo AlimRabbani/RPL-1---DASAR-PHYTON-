c8a405843a1e408174a9d62910d63141c46d070d
AlimRabbani <alimmanurung18@gmail.com>
2022-12-24 15:42:13 +0800

    programpenentuTotalHarga

---
+++ APKpenjualanBaju.py
@@ -0,0 +1,20 @@
+print ("============Selamat Datang=========")
+
+#Untuk lebih mudah dalam menentukan total harga nanti kita pakai variabel (nomo1 dst)
+
+Nomor1 = str(input("Masukkan Nama Baju : "))
+Nomor2 = int(input("Masukkan Harga Baju : "))
+Nomor3 = int(input("Masukkan Jumlah Baju Yang Di Beli :  "))
+Nomor4 = int(input("Jumlah Uang Yang Di Bayar : "))
+
+Total = (Nomor2 * Nomor3 - Nomor4 ) 
+
+print("Merek  baju yang di beli = " , Nomor1)
+print("Harga baju yang di beli = " , Nomor2) 
+print("Banyak baju yang di beli  = " , Nomor3) 
+print("Jumlah yang di bayar = " , Nomor4) 
+print("Total kembalian = Rp." , Total) 
+
+print("|============Terimakasih=============|") 
+
+
