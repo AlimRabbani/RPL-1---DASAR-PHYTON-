912d33f2f67860e358c43cd0cec2b33df04a6db3
AlimRabbani <alimmanurung18@gmail.com>
2022-12-29 19:43:34 +0800

    Hitung gaji bersih

---
+++ LatihanMenghitungGajiBersih.py
@@ -0,0 +1,20 @@
+# HITUNG GAJI BERSIH KARYAWAN 
+gaji_pokok = int(input("Masukkan gaji anda : ")) 
+lama_lembur = int(input("Lama lembur (dalam jam): "))
+
+gaji_lembur = lama_lembur * 55.000  
+
+
+gaji_bersih = gaji_pokok + gaji_lembur 
+
+print("Gaji bersih yang di terima karyawan adalah Rp." , gaji_bersih)  
+
+
+
+
+
+
+
+
+
+
